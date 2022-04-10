e1cf21dfe603c91d2f040c08317c48acb18bec52
konradsic <102176820+konradsic@users.noreply.github.com>
2022-04-10 14:27:51 +0200

    Added EconomyException. All classes in errors now inherit this class.

--- pyeconomy/errors.py
+++ pyeconomy/errors.py
@@ -1,1 +1,32 @@
-class InvalidParameter(Exception): pass
+class EconomyException(Exception):
+    """
+    Represents an economy exception.
+    
+    Used to handle most exceptions  in this project.
+
+    Arguments
+    -----------------
+        message: :class:`str`
+            An error/exception message
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        print(f"Economy function raised an error: {self.message}")
+
+class InvalidParameter(EconomyException):
+    """
+    Inherits EconomyException
+    
+    Used when invalid parameter/s are passed to a function.
+
+    Arguments
+    -----------------
+        message: :class:`str`
+            An error/exception message
+    """
+    def __init__(self, message):
+        super().__init__(message)
+
+raise InvalidParameter("wow!")
